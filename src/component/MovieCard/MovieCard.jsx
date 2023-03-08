1dbbb4ffbf897424135e74f710841e276cf99302
Sameh-andolsi <samehandolsi02@gmail.com>
2023-03-08 11:54:50 +0100

    first commit

--- src/component/MovieCard/MovieCard.jsx
+++ src/component/MovieCard/MovieCard.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import ReactStars from "react-stars";
+import { Button } from 'react-bootstrap';
 import "./MovieCard.css";
+import { Link } from 'react-router-dom';
 const MovieCard = ({movie}) => {
   return (
     <div className="composant">
       <h1 className="titre">{movie.title} </h1>
+
       <img src={movie.posterURL} alt={movie.title} className="image" />
-     <br />
-      <h5 className="desc"> {movie.description}</h5>
-      <ReactStars
-        className="stars"
-        count={5}
-        edit={false}
-        half={false}
-        value={movie.rating}
-        size={24}
-        color2={"#ffd700"}
-      />
+      <br />
+      <div>
+        <ReactStars
+          className="stars"
+          count={5}
+          edit={false}
+          half={false}
+          value={movie.rating}
+          size={24}
+          color2={"#ffd700"}
+        />
+        <Link to={`/movie/${movie.id}`}>
+          <Button variant="success" style={{ marginTop: "5%" }}>
+            Details
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
